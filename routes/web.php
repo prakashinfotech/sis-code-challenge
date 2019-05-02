dc6c5d3692598db01c2aa4d8dacb1667639e1fe3
Rashmika Patel <rashmika@prakashinfotech.com>
2019-05-02 18:34:46 +0530

    auth setup with email verification

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'HomeController@index')->name('home');
+Auth::routes();
+Auth::routes(['verify' => true]);
+Route::middleware('verified')->group(function() {
+	Route::get('/home', 'HomeController@index')->name('home');
 });
