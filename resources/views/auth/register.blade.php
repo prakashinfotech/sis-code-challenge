61c53444cb6a23b3e1160099ecd22eba66ce09c3
Rashmika Patel <rashmika@prakashinfotech.com>
2019-05-09 12:13:28 +0530

    auth view localization added

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,14 +5,14 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header"><h5>{{ __('Register') }}</h5></div>
+                <div class="card-header"><h5>{{ __('auth.Register') }}</h5></div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}<em>*</em></label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('auth.Name') }}<em>*</em></label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -26,7 +26,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}<em>*</em></label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('auth.E-Mail Address') }}<em>*</em></label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -41,7 +41,7 @@
                         
                         
                         <div class="form-group row">
-                            <label for="address" class="col-md-4 col-form-label text-md-right">{{ __('Address') }}<em>*</em></label>
+                            <label for="address" class="col-md-4 col-form-label text-md-right">{{ __('auth.Address') }}<em>*</em></label>
 
                             <div class="col-md-6">
                               <textarea class="form-control @error('address') is-invalid @enderror" rows="3" cols="5" autofocus="" required name="address">{{ old('address') }}</textarea>
@@ -55,7 +55,7 @@
         					
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}<em>*</em></label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('auth.Password') }}<em>*</em></label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -69,7 +69,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}<em>*</em></label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('auth.Confirm Password') }}<em>*</em></label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
@@ -79,7 +79,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                                    {{ __('auth.Register') }}
                                 </button>
                             </div>
                         </div>
